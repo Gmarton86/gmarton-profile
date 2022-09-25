68bb1fea2f51f84c51a417176c4078c3a3518630
Erik Zurvalec <erikzurvalec86@gmail.com>
2022-09-25 18:03:14 +0200

    feat: project view in animation

--- src/component/projects/Project.tsx
+++ src/component/projects/Project.tsx
@@ -1,26 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import BackspaceIcon from "@mui/icons-material/Backspace";
 import { IconButton } from "@mui/material";
+import { useTransition, animated } from "@react-spring/web";
 
 const Project = (props: any) => {
+  const [animation, setAnimation] = useState<boolean>(false);
+  const transitions = useTransition(animation, {
+    key: animation,
+    from: { opacity: 0, transform: "translateY(-100%)" },
+    enter: { opacity: 1, transform: "translateY(0)" },
+    config: { duration: 1000 },
+  });
+
+  useEffect(() => {
+    if (props.project.isVisible) setAnimation(true);
+  }, [props.project.isVisible]);
 
   return props.project.isVisible ? (
-    <div className="project__container">
-      <div>
-        <IconButton
-          className="icon__button"
-          aria-label="Backspace"
-          id="backspace-icon"
-          onClick={() =>
-            props.setProject((prev: any) => {
-              return { ...prev, isVisible: false };
-            })
-          }
-        >
-          <BackspaceIcon />
-        </IconButton>
-      </div>
-    </div>
+    transitions(
+      (styles, item) =>
+        item && (
+          <animated.div style={styles}>
+            <div className="project__container">
+              <div>
+                <IconButton
+                  className="icon__button"
+                  aria-label="Backspace"
+                  id="backspace-icon"
+                  onClick={() => {
+                    setAnimation(false);
+                    props.setProject((prev: any) => {
+                      return { ...prev, isVisible: false };
+                    });
+                  }}
+                >
+                  <BackspaceIcon />
+                </IconButton>
+              </div>
+            </div>
+          </animated.div>
+        )
+    )
   ) : (
     <></>
   );
