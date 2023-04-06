7e12b0126fa88f7e59f7c67aac7341af6a35ac11
ErikZurvalec <xzurvalec@stuba.sk>
2023-04-06 12:01:15 +0200

    feat: humburger menu finished

--- src/component/header/HeaderMenu.tsx
+++ src/component/header/HeaderMenu.tsx
@@ -11,12 +11,23 @@ import { useNavigate } from "react-router-dom";
 const HeaderMenu = () => {
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
   const open = Boolean(anchorEl);
+
+  const [anchorElMobileNavigation, setAnchorElMobileNavigation] =
+    React.useState<null | HTMLElement>(null);
+  const openMobileNavigation = Boolean(anchorElMobileNavigation);
+
   const { t, i18n } = useTranslation();
   const navigate = useNavigate();
 
   const options = [
     { id: "en", label: t("LANGUAGES.EN") },
     { id: "sk", label: t("LANGUAGES.SK") },
+  ];
+
+  const mobileNavigationOptions = [
+    { id: "projects", label: t("HEADER_MENU.PROJECTS") },
+    { id: "about", label: t("HEADER_MENU.ABOUT") },
+    { id: "contact", label: t("HEADER_MENU.CONTACT") },
   ];
 
   const ITEM_HEIGHT = 48;
@@ -26,6 +37,15 @@ const HeaderMenu = () => {
   };
   const handleClose = () => {
     setAnchorEl(null);
+  };
+
+  const handleClickMobileNavigation = (
+    event: React.MouseEvent<HTMLElement>
+  ) => {
+    setAnchorElMobileNavigation(event.currentTarget);
+  };
+  const handleCloseMobileNavigation = () => {
+    setAnchorElMobileNavigation(null);
   };
 
   const changeLanguage = (language: string) => {
@@ -102,14 +122,43 @@ const HeaderMenu = () => {
             aria-label="mobile navigation"
             className="header-menu-font"
             id="mobile-navigation"
-            // aria-controls={open ? "long-menu" : undefined}
-            // aria-expanded={open ? "true" : undefined}
-            // aria-haspopup="true"
-            // onClick={handleClick}
+            aria-controls={
+              openMobileNavigation ? "mobile-navigation-menu" : undefined
+            }
+            aria-expanded={openMobileNavigation ? "true" : undefined}
+            aria-haspopup="true"
+            onClick={handleClickMobileNavigation}
           >
             <MenuIcon />
           </IconButton>
         </div>
+        <Menu
+          id="mobile-navigation-menu"
+          MenuListProps={{
+            "aria-labelledby": "mobile-navigation",
+          }}
+          anchorEl={anchorElMobileNavigation}
+          open={openMobileNavigation}
+          onClose={handleCloseMobileNavigation}
+          PaperProps={{
+            style: {
+              maxHeight: ITEM_HEIGHT * 4.5,
+              width: "20ch",
+            },
+          }}
+        >
+          {mobileNavigationOptions.map((option) => (
+            <MenuItem
+              key={option.id}
+              onClick={() => {
+                navigate(`/${option.id}`);
+                handleCloseMobileNavigation();
+              }}
+            >
+              {option.label}
+            </MenuItem>
+          ))}
+        </Menu>
       </div>
     </div>
   );
